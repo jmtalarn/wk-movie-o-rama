66d904d907e92c382f973b861486b9df35e138c6
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-06-07 11:09:44 +0200

    Added some simple authentication routing

--- src/App.js
+++ src/App.js
@@ -3,52 +3,117 @@ import logo from './logo.svg';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect,
+  withRouter,
 } from 'react-router-dom'
 import './App.css';
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100) // fake async
+  }
+}
 
 const Home = () => (
   <div>
     <h2>Home</h2>
   </div>
 )
+class Login extends React.Component {
+  state = {
+    redirectToReferrer: false
+  }
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState(() => ({
+        redirectToReferrer: true
+      }))
+    })
+  }
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
 
+    if (redirectToReferrer === true) {
+      return <Redirect to={from} />
+    }
+
+    return (
+      <div>
+        <p>You must log in to view the page</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    )
+  }
+}
+const LogoutLink = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated ? (
+    
+     <Link to="/" onClick={() => {
+        fakeAuth.signout(() => history.push('/'))
+    }}>Log out</Link>
+  ) : (
+      <Link to="/login" > Login</Link >
+    )
+))
 const About = () => (
   <div>
     <h2>About</h2>
   </div>
 )
-
+// @TODO Check https://tylermcginnis.com/react-router-protected-routes-authentication/
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+       
         <Router>
         <div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
             <div>
               <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
+                <li><LogoutLink /></li>
               </ul>
 
               <hr />
 
               <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
+              <Route path="/login" component={Login} />
+              <PrivateRoute path="/about" component={About} />
             </div>
-          </p>
+
           </div>
         </Router>
       </div>
     );
   }
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }} />
+  )} />
+)
 
 export default App;
 
