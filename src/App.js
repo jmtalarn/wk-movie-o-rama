8bf26481ba8d4833e3ff945e3b4bcac405296146
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-06-07 10:44:50 +0200

    Added some basic routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
@@ -10,12 +27,28 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+        <div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+
+              <hr />
+
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+            </div>
+          </p>
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
 export default App;
+
