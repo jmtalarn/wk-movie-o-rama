18919c831f4ccae79b51daffe20d13fb147c0eab
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-06-08 11:16:18 +0200

    Created reducers for login profiles

---
+++ src/components/login/reducers/index.js
@@ -0,0 +1,23 @@
+import * as ActionTypes from '../actions/types';
+
+function loginReducer(state = {
+	profiles: [],
+	error: ''
+}, action) {
+	switch (action.type) {
+		case ActionTypes.LOGIN_PROFILES_RECEIVED:
+			return Object.assign({}, state, {
+				profiles: action.profiles,
+				error: ''
+			});
+		case ActionTypes.LOGIN_PROFILES_ERROR:
+			return Object.assign({}, state, {
+				profiles: [],
+				error: action.error
+			});
+		default:
+			return state;
+	}
+}
+
+export default authReducer;
