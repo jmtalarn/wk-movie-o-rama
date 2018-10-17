ddbb71485a72b96a4cff6c025b355fbefdd546c7
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-10-17 10:04:49 +0200

    Added more icons

--- src/FontAwesome.init.js
+++ src/FontAwesome.init.js
@@ -1,7 +1,20 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGhost, faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+	faGhost, faSignInAlt, faSignOutAlt, faHome,
+	faQuestionCircle, faFilm, faUser, faUsers, faShareAlt,
+	faHeart, faComments, faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(faGhost);
 library.add(faSignInAlt);
 library.add(faSignOutAlt);
+library.add(faHome);
+library.add(faQuestionCircle);
+library.add(faFilm);
+library.add(faUser);
+library.add(faUsers);
+library.add(faShareAlt);
+library.add(faHeart);
+library.add(faComments);
+library.add(faEnvelope);
