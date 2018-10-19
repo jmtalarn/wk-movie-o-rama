1ef8eba51d5cd9ef3e7211d032f1aae7968235a7
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-10-19 10:58:48 +0200

    Styling profile page

--- src/components/profile/view/Profile.js
+++ src/components/profile/view/Profile.js
@@ -1,32 +1,50 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Profile.css';
+
 // import {
 // 	Redirect,
 // } from 'react-router-dom';
 
 class Profile extends React.Component {
-	componentWillMount(){
+	componentWillMount() {
 		this.props.getProfile();
 	}
 
-	renderProfile(){
+	renderProfile() {
 		const {
 			profile
 		} = this.props;
-		return(
-			profile?
-				<p>
-					{profile.info.username}
-					<img
-						alt={`The ${profile.info.username} avatar`}
-						src={`/api/profiles/${profile.info.id}/avatar`}
-					/>
-				</p>:
-				null
+		console.log(profile);
+		return (
+			<div>
+				{profile.info && profile.info.id ?
+					<div className="profile-box">
+						<img
+							className="profile-avatar"
+							alt={`The ${profile.info.username} avatar`}
+							src={`/api/profiles/${profile.info.id}/avatar`}
+						/>
+						<div className="profile-details">
+							<h3 className="profile-username">
+								{profile.info.username}
+							</h3>
+							<ul className="profile-actions">
+								<li><FontAwesomeIcon icon="share-alt" /> Shared {profile.shares ? profile.shares.length : 0} messages</li>
+								<li><FontAwesomeIcon icon="heart" /> Liked {profile.likes ? profile.likes.length : 0} movies</li>
+							</ul>
+						</div>
+					</div> :
+					null
+				}
+			</div>
+
 		);
 	}
 	render() {
+
 		return (
-			<div>
+			<div className="profile-view">
 				<h2>Profile page</h2>
 				{this.renderProfile()}
 			</div>
